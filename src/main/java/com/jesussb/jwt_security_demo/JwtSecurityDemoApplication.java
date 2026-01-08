3dd6e5248082e2c4a5feaa6f522eca5d53399c3b
jesussb560 <jesus.segurab560@gmail.com>
2026-01-08 15:07:26 -0300

    first commit

---
+++ src/main/java/com/jesussb/jwt_security_demo/JwtSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jesussb.jwt_security_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSecurityDemoApplication.class, args);
+	}
+
+}
